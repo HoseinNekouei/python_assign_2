adf9ebf2d845db881f93d5352f8395a75a4c1ec5
Hosein Nekouei <nekoueihosein@gmail.com>
2022-04-09 01:24:36 +0430

    The bog of month function was fixed

--- Function.py
+++ Function.py
@@ -46,6 +46,37 @@ def check_month(str_month):
         print(error_message)
         return False,str_month
 
+# This Function Check the validity of day
+def check_day(str_day,str_month):
+    len_day=len(str_day)
+    int_day= int(str_day)
+    int_month=int(str_month)
+
+    if len_day<=2:
+        if str_day.isdigit()==True:
+            x=dayInMonth[int_month-1]
+            if int_day <= x:
+                if len_day==1:
+                    str_day="0"+str_day
+                return True,str_day
+            else:
+                error_message="The number of days entered is more than the number of days allowed this month. "
+                print(error_message)
+                return False,str_day
+        else:
+            error_message = "The value entered is not a number for the year section. "
+            print(error_message)
+            return False,str_day
+    else:
+        error_message="The length of day can not grater that 2 character. "
+        print(error_message)
+        return False,str_day
+
+
+dayInMonth = [31,31,31,31,31,31,30,30,30,30,30,29]
+dayInMonthInWord =["فروردين","ارديبهشت","خرداد","تير","مرداد","شهريور"
+                       ,"مهر","آبان","آذر","دي","بهمن","اسفند",]
+
 while True:
     date= input("\nPleae Enter a new date like yyyy-mm-dd ... ")
     #pattern validation control
@@ -54,15 +85,18 @@ while True:
 
             str_year,str_month,str_day = date.split("-")
             check_year_result = check_year(str_year)
-            check_month_result = check_month(str_month)
-            #check_day = check_day(str_day)
 
             if check_year_result == True:
+                check_month_result = check_month(str_month)
+    
                 if check_month_result[0] == True:
                     str_month=check_month_result[1]
+                    check_day_result = check_day(str_day,str_month)
                     
-                    if check_day(str_day)==True:
+                    if check_day_result[0]==True:
+                        str_day=check_day_result[1]
                         print("The date entered is approved!")
+                        print(str_year+"/"+str_month+"/"+str_day)
                     else:
                         print("Try again!")
                 else:
