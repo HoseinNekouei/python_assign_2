a2c55c9e9a1d8851b52a48107239487413ef0c12
Hosein Nekouei <nekoueihosein@gmail.com>
2022-04-13 23:53:59 +0430

    Class is created

--- Class.py
+++ Class.py
@@ -1,9 +1,21 @@
-def calc_factorial(x):
-    if x==1:
-        return x
-    else:
-        return (x * calc_factorial(x-1))
+# def calc_factorial(x):
+#     if x==1:
+#         return x
+#     else:
+#         return (x * calc_factorial(x-1))
 
-num = 4
+# num = 4
 
-print("The factorial of",num,"is",calc_factorial(num))
+# print("The factorial of",num,"is",calc_factorial(num))
+
+class MyClass:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def my_function(self):
+        print("the name is: ",self.name,"and the age is: ", self..age)
+
+
+p1=MyClass("hosein", "39")    
+p1.my_function()
