0817fdc8c4ff80d3462be1f1401b8bf5d0b048ba
Hosein Nekouei <nekoueihosein@gmail.com>
2022-04-16 23:34:21 +0430

    The Last

--- Function.py
+++ Function.py
@@ -81,7 +81,7 @@ def check_day(str_day,str_month,leap_param):
             elif leap_param==True and int_month==12:
                 if int_day <= 30:
                     return str_day,str_month, True
-            elif leap_param == true and int_month < 12:
+            elif leap_param == True and int_month < 12:
                 x=dayInMonth[int_month-1]
                 if int_day <=x:
                     return str_day,str_month, True
