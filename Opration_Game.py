6fa592ec44c581e2e54bb6a28b560faa46ac6040
Hosein Nekouei <nekoueihosein@gmail.com>
2022-06-04 20:21:19 +0430

    add score and run the game for 5 round

--- Opration_Game.py
+++ Opration_Game.py
@@ -1,16 +1,35 @@
 
+from ast import MatchStar
+
+
 MATCHSTICKS = 15 # Number of total mitch sticks in the game.    
-GAMEROUND = 0 # number of times the game has played.
+GAMEROUND = 1 # number of times the game has played.
+player1_score =0
+player2_score=0
+
 player_1 = input('The FIRST player,tell me your name...')
 player_2 = input('The SECOND player,tell me your name...')
 
-while MATCHSTICKS >= 5:
-    num = int(input('Please Take some Match Sticks Between 1 to 4 (NOM: {}): '
-                    .format(MATCHSTICKS))) # input from player.
-    MATCHSTICKS -= num
-    GAMEROUND += 1
-                        
-win_message = '{} is Win'    
-check =  lambda player : print(win_message.format(player_1) if (GAMEROUND % 2==0 and MATCHSTICKS % 2 == 0) 
-                               else print(win_message.format(player_2)))
-winner = check(GAMEROUND)
+for item in range(5):   
+    while MATCHSTICKS >= 5:
+        if GAMEROUND % 2 == 0: player_name = player_2
+        else : player_name = player_1 
+
+        num = int(input('Please Take some Match Sticks Between 1 to 4 (Turn: {0} / NOM: {1}): '
+                        .format(player_name,MATCHSTICKS))) # input from player.
+        MATCHSTICKS -= num
+        GAMEROUND += 1
+    
+    # find winner                        
+    if GAMEROUND % 2==0 and MATCHSTICKS % 2 == 0:
+        print(F'{player_2} is WIN!')
+        player2_score += 1
+    else:
+        print(F'{player_1} is WIN!')
+        player1_score += 1        
+
+    MATCHSTICKS = 15 # Reset repository for a new round
+    GAMEROUND = 1 # reset game round
+    print('try againfor a new round ...')
+    
+print(F'{player_1} Score is: ',player1_score + 'VS' + F'{player_2} Score is: ',player2_score)
