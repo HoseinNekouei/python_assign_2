a40123e42b7234bee175b9a6ad3096cc58be8a47
Hosein Nekouei <nekoueihosein@gmail.com>
2022-06-04 14:05:35 +0430

    This is the first

---
+++ Opration_Game.py
@@ -0,0 +1,20 @@
+
+from email import message
+
+
+MATCHSTICKS = 15 # Number of total mitch sticks in the game.    
+GAMEROUND = 0 # number of times the game has played.
+player_1 = input('The FIRST player,tell me your name...')
+player_2 = input('The SECOND player,tell me your name...')
+
+for item in range(10):
+    if MATCHSTICKS >= 5:
+        num = int(input('Please Take some Match Sticks Between 1 to 4 (NOM: {}): '
+                        .format(MATCHSTICKS))) # input from player.
+        MATCHSTICKS -= num
+        GAMEROUND += 1
+                        
+win_message = '{} is Win'    
+check =  lambda player : print(win_message.format(player_1) if (GAMEROUND % 2==0 and MATCHSTICKS % 2 == 0) 
+                               else print(win_message.format(player_2)))
+winner = check(GAMEROUND)
