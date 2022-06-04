a88f9babc58515b2de9b453e114032abb86c5232
Hosein Nekouei <nekoueihosein@gmail.com>
2022-06-04 22:08:18 +0430

    Done!

--- Opration_Game.py
+++ Opration_Game.py
@@ -16,29 +16,27 @@ for item in range(5):
         MATCHSTICKS -= num
         GAMEROUND += 1
     
-    myList = {player_1:player1_score,player_2:player2_score} # create a didtionary
+    myList = [F'{player_1}:{player1_score}', F'\n{player_2}:{player2_score}'] # create a list to prepare for saving in the file
 
     # find winner
     if GAMEROUND % 2==0 and MATCHSTICKS % 2 == 0:
         print(F'{player_2} is WIN!')
         player2_score += 1
-        myList.update({player_2:player2_score})
+        myList[1]=F'\n{player_2}:{player2_score}'
     else:
         print(F'{player_1} is WIN!')
         player1_score += 1        
-        myList.update({player_1:player1_score})
+        myList[0]=F'{player_1}:{player1_score}'
 
     # Write the players score in a file
     with open('c:\\development\\workspace\\python\\git\\python_assign_2\\gamescore.txt','w') as scoreWrite:
         scoreWrite.writelines(myList)
-
-    with open('c:\\development\\workspace\\python\\git\\python_assign_2\\gamescore.txt','r') as scoreRead:
-        playerScoreFile = scoreRead.readline()
-        for item in playerScoreFile:
-            print(playerScoreFile)
         
     MATCHSTICKS = 15 # Reset repository for a new round
     GAMEROUND = 1 # reset game round
-    if item <5 : print('try againfor a new round ...')
+    print('Try again for a new round ...') if item < 4 else print('Game Over!')
     
-print((F'{player_1} Score is: ',player1_score) + 'VS' + (F'{player_2} Score is: ',player2_score))
+with open('c:\\development\\workspace\\python\\git\\python_assign_2\\gamescore.txt','r') as scoreRead:
+    playerScoreFile = scoreRead.readlines()
+    for item in playerScoreFile:
+        print(item)
